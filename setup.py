4ae717320368887ba3f1639637496272ddf1e7ab
Shlok Kothari <shlok.kothari@gmail.com>
2025-03-04 19:45:31 -0500

    added flake8, black and pre-commit and setup.cfg support

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="project-euler-utils",
+    author="Shlok Kothari",
+    description="Utility functions to solve project-euler and math problems on computer",
+    version="0.1.0",
+    install_requires=['numpy>=2.0'],
+    python_requires='>3.8.*'
+)
